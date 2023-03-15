e7d38a84f04a9295940428166f99d06e66b37e1b
Adarsh Kumar <adarsh010c@gmail.com>
2023-03-16 05:03:46 +0530

    Added the Home component

--- src/Front.jsx
+++ src/Front.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./Header";
+import Home from "./Home";
 
 export default function Front(){
     return(
         <div className="front">
             <Header />
+            <Home />
         </div>
     )
 }
---
+++ src/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Home(){
+    return(
+        <div className="home">
+            <h1 className="home__head">
+            You got the travel plans, we got the travel vans.
+            </h1>
+            <h3 className="home__description">
+            Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.
+            </h3>
+            <a href="#" className="home__btn">
+            Find your van
+            </a>
+        </div>
+    )
+}
