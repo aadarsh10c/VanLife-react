f970a0f7cc19ff0c76b5e5800558509af025cfff
Adarsh Kumar <adarsh010c@gmail.com>
2023-03-21 13:29:19 +0530

    Completed the static Dashboard

--- src/pages/Host/Dashboard.jsx
+++ src/pages/Host/Dashboard.jsx
@@ -1,7 +1,47 @@
 import React from "react";
-
+import starURL from '../../assets/Star_3.png'
 export default function Dashboard(){
     return(
-        <h1>Dashboard is here</h1>
+        <div className="dashboard">
+            <div className="profile">
+                <h1 className="profile__head">
+                    Welcome!
+                </h1>
+                <div className="profile__details">
+                    <p className="profile__details-month">
+                        Income last <span>30days</span>
+                    </p>
+                    <h4 className="profile__details-detail">
+                        Details
+                    </h4>
+                </div>
+                <h1 className="profile__price">
+                    $2,260
+                </h1>
+            </div>
+            <div className="review">
+                <div className="review__score">
+                    <h3 className="review__score-head">
+                        Review Score
+                    </h3>
+                    <div className="review__score-rating">
+                        <img src={starURL} alt="star" />
+                        <p><span>5.0</span>/5</p>
+                    </div>
+                </div>
+                <h4 className="review__detail">
+                    Details
+                </h4>
+            </div>
+            <div className="vanlist">
+                <div className="head">
+                    <h3 className="head__1">Your listed vans</h3>
+                    <p className="head__2">View all</p>
+                </div>
+                <div className="container">
+                    <h1>Van List</h1>
+                </div>
+            </div>
+        </div>
     )
 }
